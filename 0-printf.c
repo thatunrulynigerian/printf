192caf09614fe73e12f19fb2d9e632f49e86d102
Martin Okem Odachi <odachi054@gmail.com>
2023-12-02 03:59:59 -0800

    created the main.h file

---
+++ 0-printf.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - function that prints a formated string
+ * @format: string of formatted characters
+ * Return:Number of character printed
+ **/
+
+int _printf(const char *format, ...)
+{
+	int countC = 0;
+	va_list args_lists;
+
+	va_start(arg_lists, format);
+
+	return (countC);
+}
