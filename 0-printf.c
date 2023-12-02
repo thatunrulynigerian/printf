ab168c849ed32067afd7693a11d24196781ffd43
Martin Okem Odachi <odachi054@gmail.com>
2023-12-02 08:26:54 -0800

    created a main.c file created my task 0 file, compiled without errors give expected output updated main.h

--- 0-printf.c
+++ 0-printf.c
@@ -10,9 +10,49 @@
 int _printf(const char *format, ...)
 {
 	int countC = 0;
-	va_list args_lists;
+	va_list arg_lists;
 
 	va_start(arg_lists, format);
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			countC++;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char sc = va_arg(arg_lists, int);
 
+				write(1, &sc, 1);
+				countC++;
+			}
+			else if (*format == 's')
+			{
+				char *strc = va_arg(arg_lists, char*);
+
+				if (strc == NULL)
+					return (-1);
+				while (*strc)
+				{
+					write(1, strc, 1);
+					strc++;
+					countC++;
+				}
+			}
+			else
+			{
+				write(1, format - 1, 1);
+				countC++;
+			}
+		}
+		format++;
+	}
+	va_end(arg_lists);
 	return (countC);
 }
