3a780387cbf16e6fd5767c03c556974d13a69bfa
Martin Okem Odachi <odachi054@gmail.com>
2023-12-05 20:25:37 -0800

    new

--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 #include <unistd.h>
 #include <stdarg.h>
+#include "helperFunction.h"
+void default_handler(char *format, int *countC);
+void print_str(const char *strc);
+void print_sc(char sc)
 int _printf(const char *format, ...);
 
 #endif
